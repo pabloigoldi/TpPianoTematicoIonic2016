49c2e2f47f441173628ce173466747f02bfe70c0
pabloigoldi <pablo.emanuel.ig@gmail.com>
2016-10-15 02:02:13 -0300

    ultimo

--- www/js/app.js
+++ www/js/app.js
@@ -3,14 +3,15 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
@@ -22,52 +23,54 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
+ $ionicConfigProvider.navBar.alignTitle('center');
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
   $stateProvider
 
-    .state('app', {
-    url: '/app',
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
     abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+    templateUrl: 'templates/tabs.html'
   })
 
-  .state('app.search', {
-    url: '/search',
+  // Each tab has its own nav history stack:
+
+  .state('tab.instrumentos', {
+    url: '/instrumentos',
     views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
+      'instrumentos': {
+        templateUrl: 'templates/instrumentos.html',
+        controller: 'instrumentosCtrl'
       }
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
+     .state('tab.info', {
+       url: '/info',
+        views: {
+          'menuInfo': {
+           templateUrl: 'templates/info.html',
+            controller:'infoCtrl' 
+              }
         }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+      })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+  .state('tab.login', {
+      url: '/login',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/login.html',
+          controller: 'loginCtrl'
+        }
       }
-    }
-  });
+    });
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/tab/login');
+
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,229 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('instrumentosCtrl', function($scope,$cordovaNativeAudio, 
+          $cordovaVibration,$ionicPlatform,$cordovaFile,$state) {
+     var arrySecuencia=[];
+     var arrayJSON;
+    
+    $ionicPlatform.ready(function(){
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+      $scope.mostrarInstrumentos = true;
+       $scope.showBtnGuardar = true;
+      $scope.showBtnVolver = false;
 
-  // Form data for the login modal
+
+ // $scope.arrySecuencia = "";
+ // $scope.lista= "";
+
+       try{
+    
+           $cordovaFile.createFile(cordova.file.externalApplicationStorageDirectory, "sonidosPiano.txt", true)
+         .then(function (success) {
+           alert("archivo creado"+path);
+        }, function (error) {
+           alert("archivo no creado"+path);
+      });
+
+       $cordovaNativeAudio
+      .preloadSimple('guitarra', 'sonidos/guitarraElectrica.mp3')
+      .then(function (msg) {
+        console.log(msg);
+      }, function (error) {
+        console.error(error);
+       }); 
+         $cordovaNativeAudio
+      .preloadSimple('flauta', 'sonidos/flauta.mp3')
+      .then(function (msg) {
+        console.log(msg);
+      }, function (error) {
+        console.error(error);
+       }); 
+      $cordovaNativeAudio
+      .preloadSimple('arpa', 'sonidos/arpa.mp3')
+      .then(function (msg) {
+        console.log(msg);
+      }, function (error) {
+        console.error(error);
+       }); 
+     $cordovaNativeAudio
+      .preloadSimple('platillos', 'sonidos/platillos.mp3')
+      .then(function (msg) {
+        console.log(msg);
+      }, function (error) {
+        console.error(error);
+       }); 
+     $cordovaNativeAudio
+      .preloadSimple('violin', 'sonidos/violin.mp3')
+      .then(function (msg) {
+        console.log(msg);
+      }, function (error) {
+        console.error(error);
+       });  
+     $cordovaNativeAudio
+      .preloadSimple('piano', 'sonidos/piano.mp3')
+      .then(function (msg) {
+        console.log(msg);
+      }, function (error) {
+        console.error(error);
+       });
+       } catch(ex){
+        alert(ex);
+       }
+
+
+    });
+
+   $scope.reproducir=function(rep){
+         
+
+    if(rep==0){
+         arrySecuencia.push("guitarra");
+          //$cordovaVibration.vibrate(500);
+     $cordovaNativeAudio.play('guitarra');
+    }
+        if(rep==1){   
+         arrySecuencia.push("flauta");
+        //  $cordovaVibration.vibrate(500);
+        $cordovaNativeAudio.play('flauta');
+    
+    }
+        if(rep==2){
+           arrySecuencia.push("arpa");
+        //  $cordovaVibration.vibrate(500);
+          $cordovaNativeAudio.play('arpa');
+    
+    }
+    if(rep==3){
+ 
+        arrySecuencia.push("platillos");
+        // $cordovaVibration.vibrate(500);
+        $cordovaNativeAudio.play('platillos');
+    
+   
+    }
+        if(rep==4){
+     arrySecuencia.push("violin");
+     //$cordovaVibration.vibrate(500);
+       $cordovaNativeAudio.play('violin');
+   
+    }
+        if(rep==5){
+        arrySecuencia.push("piano");
+     //$cordovaVibration.vibrate(500);
+       $cordovaNativeAudio.play('piano');
+
+    }
+
+     $scope.arrySecuencia = arrySecuencia;
+
+    arrayJSON=JSON.stringify(arrySecuencia);
+    
+    try{
+           $cordovaFile.createFile(cordova.file.externalApplicationStorageDirectory, "SecuenciaPiano.txt", true)
+      .then(function (success) {
+  
+      }, function (error) {
+        // error
+      });
+        $cordovaFile.writeFile(cordova.file.externalApplicationStorageDirectory, "SecuenciaPiano.txt", arrayJSON,true)
+      .then(function (success) {
+      }, function (error) {
+      });
+    }
+    catch(ex){
+      alert(ex);
+    }
+
+   };
+
+  $scope.repSecuencia=function(){
+
+      $scope.mostrarInstrumentos = false;
+      $scope.showBtnGuardar = false;
+      $scope.showBtnVolver = true;
+
+      var fecha = new Date();
+      var fechaActual =  fecha.getDay()+ "-"+fecha.getMonth() + "-" +fecha.getFullYear() ;
+      $scope.lista= "Instrumentos tocados:" + $scope.arrySecuencia;
+      var ref = new Firebase("https://practicasupervisada2016-f74f7.firebaseio.com/pianito");
+      ref.push({
+          usuario:firebase.auth().currentUser.email,     
+          fecha:fechaActual,
+          listaMelodias :$scope.arrySecuencia
+      });
+  
+
+      var array=[];
+      $cordovaFile.readAsText(cordova.file.externalApplicationStorageDirectory, "SecuenciaPiano.txt")
+      .then(function (success) {
+       array=JSON.parse(success);  
+          $state.go('tab.secuencia',{nombres:array});   
+
+         }, function (error) {
+        alert("error");
+      });
+  };
+
+  $scope.volverJugar=function(){
+      $scope.lista= "";
+      $scope.arrySecuencia = [];
+      arrySecuencia = [];
+            $scope.mostrarInstrumentos = true;
+       $scope.showBtnGuardar = true;
+      $scope.showBtnVolver = false;  }
+
+
+})
+
+.controller('infoCtrl', function($scope){
+    $scope.showBrowser=function(){
+      var ref = window.open('https://github.com/pabloigoldi/TpPianoTematicoIonic2016', '_blank', 'location=yes');
+      ref.addEventListener('loadstart', function() { alert(event.url); });
+    };
+})
+          
+
+.controller('loginCtrl', function($scope, $state) {
+  
   $scope.loginData = {};
+  $scope.loginData.username ="pablo.emanuel.ig@gmail.com";
+  $scope.loginData.password = "hola06";
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+  $scope.doLogin= function()
+  {
+      firebase.auth().signInWithEmailAndPassword($scope.loginData.username,$scope.loginData.password).catch(function(error)
+        {          
+          console.info("error: " + error);
+           var errorCode = error.code;
+           var errorMessage = error.message;
+          if(errorCode== 'auth/wrong-password')
+          {
+            $scope.mensajeLogin = "wrong-password";
+          }
+          else
+          {
+          $scope.mensajeLogin = "errores:" + errorMessage;
+          }
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        }).then(function(respuesta){
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+              if(respuesta)
+              {             
+                  $state.go('tab.instrumentos');
+              }
+              else
+              {
+                   $scope.mensajeLogin = "Error en el logueo";
+              }
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+        });
+  }
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+  $scope.salir= function()
+  {
+      ionic.Platform.exitApp();
+  }
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+
 });
